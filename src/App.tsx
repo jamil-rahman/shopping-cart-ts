963efb79242b06b4db0007f05b961d82cb44b3d2
Jamilur Rahman <r.jamilur96@gmail.com>
2022-09-03 18:54:01 +0600

    nav init

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { Container } from 'react-bootstrap'
-import {Routes, Route} from 'react-router-dom'
-import About from './pages/About/About'
-import Home from './pages/Home/Home'
-import Store from './pages/Store/Store'
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/Navbar/Navbar";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Store from "./pages/Store/Store";
 
-function App() { 
+function App() {
   return (
-    <Container>
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/store" element={<Store />} />
-      <Route path="/about" element={<About />} />
-      </Routes>
-    </Container>
-  )
+    <>
+      <NavBar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar/NavBar.tsx
@@ -0,0 +1,11 @@
+import { Container, Navbar } from 'react-bootstrap'
+
+export default function NavBar() {
+  return (
+    <Navbar className='bg-white shadow-md mb-3'>
+        <Container>
+            Navbar
+        </Container>
+    </Navbar>
+  )
+}
